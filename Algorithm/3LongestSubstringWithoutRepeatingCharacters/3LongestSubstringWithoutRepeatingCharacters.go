12e3180fdf82f25f5d7c3aac3c567b717e03f72d
kalvin <daidekun@tal.com>
2021-10-12 16:54:40 +0800

    finished algorithm 3

--- Algorithm/3LongestSubstringWithoutRepeatingCharacters/3LongestSubstringWithoutRepeatingCharacters.go
+++ Algorithm/3LongestSubstringWithoutRepeatingCharacters/3LongestSubstringWithoutRepeatingCharacters.go
@@ -1,1 +1,30 @@
 package algorithm
+
+func lengthOfLongestString(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+
+	var bitSet [256]bool
+	result, left, right := 0, 0, 0
+
+	for i := 0; i <= len(s); i++ {
+		if bitSet[s[right]] {
+			bitSet[s[left]] = false
+			left++
+		} else {
+			bitSet[s[right]] = true
+			right++
+		}
+
+		if result < right-left {
+			result = right - left
+		}
+
+		if left+result >= len(s) && right >= len(s) {
+			break
+		}
+	}
+
+	return result
+}
--- Algorithm/3LongestSubstringWithoutRepeatingCharacters/3LongestSubstringWithoutRepeatingCharacters_test.go
+++ Algorithm/3LongestSubstringWithoutRepeatingCharacters/3LongestSubstringWithoutRepeatingCharacters_test.go
@@ -1,1 +1,19 @@
 package algorithm
+
+import (
+	"fmt"
+	"testing"
+)
+
+type question2 struct {
+	para2 string
+	ans2  int
+}
+
+func Test_Problem2(T *testing.T) {
+	q := question2{
+		para2: "pwwkew",
+		ans2:  3,
+	}
+	fmt.Printf("【问题】%v,【输出】%v\n", q, lengthOfLongestString(q.para2))
+}
