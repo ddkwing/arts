0b452171cac1d4c49844aaef078a6895f2eb1a5b
kalvin <daidekun@tal.com>
2021-10-19 11:25:50 +0800

    [Algorithm]完成0141双指针找环问题

---
+++ Algorithm/141LinkedListCycle/LinkedListCycle.go
@@ -0,0 +1,21 @@
+package algorithm
+
+import (
+	"ddkwing/arts/algorithm/structures"
+)
+
+type ListNode = structures.ListNode
+
+func hasCycle(head *ListNode) bool {
+	fast := head
+	slow := head
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
